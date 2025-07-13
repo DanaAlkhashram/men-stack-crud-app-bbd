c698c5934980b07aa4fa0155f4f93b776ec0a5cc
Dana <danalkhashram@gmail.com>
2025-07-13 11:32:31 +0300

    creating route

---
+++ server.js
@@ -0,0 +1,15 @@
+// Here is where we import modules
+// We begin by loading Express
+const express = require('express');
+const app = express();
+
+// GET ----------------------------------------------------------------- 
+// ( path , callback)
+app.get('/',(req, res)=>{
+    res.send('hello')
+})
+
+// live ----------------------------------------------------------------- 
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
